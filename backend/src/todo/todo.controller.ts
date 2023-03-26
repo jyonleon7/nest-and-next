2f50ada3fc7a5b53cfe48bb1eda3fe79df0d890e
jyonleon7 <jyonleon7@gmail.com>
2023-03-26 21:53:12 +0900

    crud 処理の実装

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Task } from '@prisma/client';
+import { Request } from 'express';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { UpdateTaskDto } from './dto/update-task-dto';
+import { TodoService } from './todo.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getTasks(@Req() req: Request): Promise<Task[]> {
+    return await this.todoService.getTasks(req.user.id);
+  }
+
+  @Get(':id')
+  async getTaskById(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+  ): Promise<Task> {
+    return await this.todoService.getTaskById(id, req.user.id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  async createTask(
+    @Req() req: Request,
+    @Body() createTaskDto: CreateTaskDto,
+  ): Promise<Task> {
+    return await this.todoService.createTask(req.user.id, createTaskDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  async updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return await this.todoService.updateTask(id, req.user.id, updateTaskDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  async deleteTaskById(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+  ): Promise<void> {
+    await this.todoService.deleteTaskById(id, req.user.id);
+  }
+}
--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Task } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { UpdateTaskDto } from './dto/update-task-dto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createTask(
+    userId: number,
+    createTaskDto: CreateTaskDto,
+  ): Promise<Task> {
+    try {
+      const task = await this.prisma.task.create({
+        data: {
+          ...createTaskDto,
+          userId,
+        },
+      });
+      return task;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prisma.task.findMany({
+      where: { userId: userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  async getTaskById(id: number, userId: number): Promise<Task> {
+    const task = await this.prisma.task.findFirst({
+      where: { id, userId },
+    });
+    if (!task) {
+      throw new NotFoundException('該当のリストが見つかりませんでした。');
+    }
+    return task;
+  }
+
+  async updateTask(
+    id: number,
+    userId: number,
+    updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    const task = await this.prisma.task.findUnique({ where: { id } });
+    if (!task || task.userId !== userId) {
+      throw new ForbiddenException('no permission exception');
+    }
+    return await this.prisma.task.update({
+      where: {
+        id,
+      },
+      data: { ...updateTaskDto },
+    });
+  }
+
+  async deleteTaskById(id: number, userId: number): Promise<void> {
+    const task = await this.prisma.task.findUnique({
+      where: { id },
+    });
+
+    if (!task || task.userId !== userId) {
+      throw new ForbiddenException('No permission to delete');
+    }
+
+    await this.prisma.task.delete({ where: { id } });
+  }
+}
