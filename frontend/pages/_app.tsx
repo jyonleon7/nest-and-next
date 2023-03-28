0b341b4922f59515381b60c5403ff862efef87b7
jyonleon7 <jyonleon7@gmail.com>
2023-03-28 16:44:54 +0900

    react-query の設定

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,37 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // react-query の標準だと、RESTへのfetch に失敗した場合には、3回までtry してしまうから、それを無効にする
+      retry: false,
+      // ユーザーがブラウザにフォーカスするタイミングでfetchが走ってしまうのを阻止する
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        /** Put your mantine theme override here */
-        colorScheme: 'light',
-      }}
-    >
-      <Component {...pageProps} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 export default MyApp
